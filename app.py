7dddb874ebeadd35c2684b3b41f2a8c2d397a3fb
pessaicow <92889185+pessaicow@users.noreply.github.com>
2022-02-06 17:17:37 -0300

    Fix unit test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hello World - [G32]Thiago"
+    return "Hello World - [G32]Thiago - FIAP"
 
 if __name__ == '__main__':
     app.run()
